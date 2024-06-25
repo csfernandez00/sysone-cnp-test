083a9f484cc7385b7eb61c762d80fb635b0679a0
Carlos Fernandez <csfernandez00@gmail.com>
2024-06-25 13:12:49 -0300

    Cambio estilos

--- assets.js
+++ assets.js
@@ -10,11 +10,8 @@ export const styles = `
 
     .widget__container {
         box-shadow: 0 0 18px 8px rgba(0, 0, 0, 0.1), 0 0 32px 32px rgba(0, 0, 0, 0.08);
-        width: 400px;
+        width: 100%;
         overflow: auto;
-        right: -25px;
-        bottom: 75px;
-        position: absolute;
         transition: max-height .2s ease;
         font-family: Helvetica, Arial ,sans-serif;
         background-color: #e6e6e6a6;
--- main.js
+++ main.js
@@ -24,47 +24,47 @@ class MessageWidget {
 		/**
 		 * Create and append a div element to the document body
 		 */
-		const container = document.querySelector("#cnp-script");
+		// const container = document.getElementById("cnp-script");
 		// container.style.position = "fixed";
 		// Object.keys(this.position).forEach(
 		// 	(key) => (container.style[key] = this.position[key])
 		// );
-		document.body.appendChild(container);
+		//document.body.appendChild(container);
 
 		/**
 		 * Create a button element and give it a class of button__container
 		 */
-		const buttonContainer = document.createElement("button");
-		buttonContainer.classList.add("button__container");
+		// const buttonContainer = document.createElement("button");
+		// buttonContainer.classList.add("button__container");
 
 		/**
 		 * Create a span element for the widget icon, give it a class of 'widget__icon', update it's innerHTML property to an icon which would serve as the widget icon.
 		 */
-		const widgetIconElement = document.createElement("span");
-		widgetIconElement.innerHTML = MESSAGE_ICON;
-		widgetIconElement.classList.add("widget__icon");
-		this.widgetIcon = widgetIconElement;
+		// const widgetIconElement = document.createElement("span");
+		// widgetIconElement.innerHTML = MESSAGE_ICON;
+		// widgetIconElement.classList.add("widget__icon");
+		// this.widgetIcon = widgetIconElement;
 
 		/**
 		 * Create a span element for the close icon, give it a class of 'widget__icon' and 'widget__hidden' which would be removed whenever the widget is closed, update it's innerHTML property to an icon which would serve as the widget icon during that state.
 		 */
-		const closeIconElement = document.createElement("span");
-		closeIconElement.innerHTML = CLOSE_ICON;
-		closeIconElement.classList.add("widget__icon", "widget__hidden");
-		this.closeIcon = closeIconElement;
+		// const closeIconElement = document.createElement("span");
+		// closeIconElement.innerHTML = CLOSE_ICON;
+		// closeIconElement.classList.add("widget__icon", "widget__hidden");
+		// this.closeIcon = closeIconElement;
 
 		/**
 		 * Append both icons created to the button element and add a `click` event listener on the button to toggle the widget open and close.
 		 */
-		buttonContainer.appendChild(this.widgetIcon);
-		buttonContainer.appendChild(this.closeIcon);
-		buttonContainer.addEventListener("click", this.toggleOpen.bind(this));
+		// buttonContainer.appendChild(this.widgetIcon);
+		// buttonContainer.appendChild(this.closeIcon);
+		// buttonContainer.addEventListener("click", this.toggleOpen.bind(this));
 
 		/**
 		 * Create a container for the widget and add the following classes:- "widget__hidden", "widget__container"
 		 */
-		this.widgetContainer = document.createElement("div");
-		this.widgetContainer.classList.add("widget__hidden", "widget__container");
+		this.widgetContainer = document.getElementById("cnp-script");
+		this.widgetContainer.classList.add("widget__container");
 
 		/**
 		 * Invoke the `createWidget()` method
